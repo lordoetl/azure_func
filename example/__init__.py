7e125626e86222d86f8a3a250941ef1e9eae4975
Anthony Taylor <lordoetl@gmail.com>
2021-11-09 19:55:25 -0600

    initial

---
+++ example/__init__.py
@@ -0,0 +1,36 @@
+import logging
+import json
+import time
+import pyodbc
+import datetime
+
+import azure.functions as func
+from configparser import ConfigParser
+
+def default(o):
+    if isinstance(o,(datetime.datetime, datetime.date)):
+        return o.isoformat()
+
+
+
+#Initial script file must have main
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    #read in params
+    table_name=req.params.get('table_name')
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
